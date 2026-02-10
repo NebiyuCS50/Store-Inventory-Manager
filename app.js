c3e0133116fa785d9d0e22c2f571df635a38e051
Nebiyu <nebiyueliasonlycourses@gmail.com>
2026-02-10 18:57:23 +0300

    feat: implement initial server setup with Express and routing

---
+++ app.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const { router } = require("./Routes/index");
+
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
