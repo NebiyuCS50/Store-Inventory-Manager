299b481c6064e14bba49f9da7c5a35386882da47
Nebiyu <nebiyueliasonlycourses@gmail.com>
2026-02-10 19:00:15 +0300

    feat: implement product management functions in query.js

---
+++ db/query.js
@@ -0,0 +1,35 @@
+const pool = require("./pool");
+
+async function getAllProduct() {
+  const res = await pool.query("SELECT * FROM products");
+  return res.rows;
+}
+async function getProductById(id) {
+  const res = await pool.query("SELECT * FROM products WHERE id = $1", [id]);
+  return res.rows[0];
+}
+async function createProduct(name, price, quantity, category_id) {
+  const res = await pool.query(
+    "INSERT INTO products (name, price, quantity, category_id) VALUES ($1, $2, $3, $4) RETURNING *",
+    [name, price, quantity, category_id],
+  );
+  return res.rows[0];
+}
+async function updateProduct(id, name, price, quantity, category_id) {
+  const res = await pool.query(
+    "UPDATE products SET name = $1, price = $2, quantity = $3, category_id = $4 WHERE id = $5 RETURNING *",
+    [name, price, quantity, category_id, id],
+  );
+  return res.rows[0];
+}
+async function deleteProduct(id) {
+  await pool.query("DELETE FROM products WHERE id = $1", [id]);
+}
+
+module.exports = {
+  getAllProduct,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
