ea5c3ec28b2797ccfe0de8aec88d1dd6c77899ae
Nebiyu <nebiyueliasonlycourses@gmail.com>
2026-02-10 19:00:32 +0300

    feat: add product routes for CRUD operations in index.js

---
+++ Routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const productController = require("../Controllers/productController");
+const router = Router();
+
+router.get("/", productController.getProducts);
+router.get("/products/:id", productController.showProductDetails);
+router.get("/products/new", productController.showProductForm);
+router.post("/products", productController.createProduct);
+router.get("/products/:id/edit", productController.showEditForm);
+router.post("/products/:id/edit", productController.updateProduct);
+router.post("/products/:id/delete", productController.deleteProduct);
+
+module.exports = { router };
